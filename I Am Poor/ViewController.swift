1fc35b5499bcfebb9ff60976e82f9e44c092a304
Derin Gbadebo <daireenonline@yahoo.com>
2019-11-09 21:21:28 -0800

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Derin Gbadebo on 11/9/19.
+//  Copyright © 2019 Derin Gbadebo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
